f3be817b3c9df09409b9bebcfb2b7e6b8e9c0c83
temilolu97 <temiloluwaabiodunojo@gmail.com>
2024-05-06 14:10:25 +0100

    changes

--- src/pages/ExchangeRate.jsx
+++ src/pages/ExchangeRate.jsx
@@ -65,7 +65,7 @@ const ExchangeRate = () => {
           }
           fetchRates()
     },[])
-
+ 
     // const pairedRates = [];
     // rates.forEach((rate) => {
     //     const inverseRate = {
